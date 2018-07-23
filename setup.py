0c92b5add9e2732fd9e96e6961d7fce38d472620
Steve Kaliski <sjkaliski@users.noreply.github.com>
2018-07-23 11:27:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
   name='frameioclient',
-  version='0.0.1',
+  version='0.2.0',
   description='Client library for the Frame.io API',
   long_description=long_description,
   long_description_content_type="text/markdown",
