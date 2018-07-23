8c3b201cdf383eb3958f53f34cb453c91c2ee7b8
= <sjkaliski@gmail.com>
2018-07-23 11:24:55 -0400

    update setup.py and prepare for pip release

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setuptools.setup(
   name='frameioclient',
   version='0.0.1',
   description='Client library for the Frame.io API',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   url='https://github.com/frameio/python-frameio-client',
+  packages=setuptools.find_packages(),
   author='Frame.io, Inc.',
-  author_email='oss@frame.io',
-  license='MIT',
-  install_requires=[
-    'requests'
-  ]
+  author_email='platform@frame.io',
+  license='MIT'
 )
