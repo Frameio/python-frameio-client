2840277b5eb32b8e5bd0123fed9859c24bc17889
= <sjkaliski@gmail.com>
2018-10-15 09:56:55 -0400

    bump to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
   name='frameioclient',
-  version='0.3.1',
+  version='0.4.0',
   description='Client library for the Frame.io API',
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -15,4 +15,3 @@ setuptools.setup(
   author_email='platform@frame.io',
   license='MIT'
 )
-
