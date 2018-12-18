4afcbd824898900ae0a756bcff8ea8a642368a62
Michael Guarino <mguarino@frame.io>
2018-12-18 11:38:50 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
   name='frameioclient',
-  version='0.4.0',
+  version='0.5.0',
   description='Client library for the Frame.io API',
   long_description=long_description,
   long_description_content_type="text/markdown",
