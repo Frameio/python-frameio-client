4231f25fd5bef6310db72e4e18cfc578996095cb
makseq-msi3 <makseq@gmail.com>
2020-03-02 02:55:52 +0300

    Export to excel format.

--- todo-table/build_table.py
+++ todo-table/build_table.py
@@ -3,6 +3,37 @@ import os
 import json
 
 
+def write_xlsx(table):
+    """ Export to excel
+    """
+    import xlsxwriter
+    import io
+    import urllib.request
+
+    workbook = xlsxwriter.Workbook('web/output.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    # Widen the first column to make the text clearer.
+    worksheet.set_column('A:A', 7)  # time
+    worksheet.set_column('B:B', 36)  # image
+    worksheet.set_column('C:C', 40)  # desc
+    worksheet.set_column('D:D', 10)  # assign
+    top = workbook.add_format({'valign': 'top', 'text_wrap': True})
+
+    for i, t in enumerate(table):
+        k = i + 1
+        # Insert an image
+        print('A%i' % k)
+        worksheet.write_string('A%i' % k, t['timestamp'], top)
+        image_data = io.BytesIO(urllib.request.urlopen(t['image']).read())
+        worksheet.insert_image('B%i' % k, t['image'], {'image_data': image_data, 'x_scale': 0.171, 'y_scale': 0.2})
+        worksheet.write_string('C%i' % k, t['desc'], top)
+        worksheet.write_string('D%i' % k, ','.join(t['assigned']), top)
+        worksheet.set_row(i, 200)
+
+    workbook.close()
+
+
 def main():
     client = FrameioClient(open('token.txt').read())
     me = client.get_me()
@@ -63,6 +94,8 @@ def main():
         })
 
     table = sorted(table, key=lambda k: k['frame'])
+    write_xlsx(table)
+
     json.dump(comments, open('cache_all.json', 'w'))
     json.dump(table, open('web/table.json', 'w'))
 
