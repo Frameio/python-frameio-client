48f83b6dc39200d2374015c62c58feb1a09dd9eb
kylenstone <kylenstone@users.noreply.github.com>
2020-05-17 13:12:19 -0400

    Add conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
+import os
+
 from frameioclient import FrameioClient
 
-@pytest.fixture
-def frameioclient(token):
-  return FrameioClient("aaaabbbbccccddddeeee")
+token = os.getenv('FRAME_IO_TOKEN')
+
+@pytest.fixture()
+def setup_client():
+  client = FrameioClient(token)
+  return client
+  
