0de0dca042c13773c5a9cb22835864076b79a616
Max Stromberg <m@maxstr.se>
2020-07-02 18:44:27 +0200

    improve downloader ram usage and make replace optional

--- frameioclient/download.py
+++ frameioclient/download.py
@@ -1,18 +1,27 @@
 import requests
-import math
 import os
 
 class FrameioDownloader(object):
-  def __init__(self, asset, download_folder):
+  def __init__(self, asset, download_folder, replace):
     self.asset = asset
     self.download_folder = download_folder
+    self.replace = replace
 
   def download(self):
     original_filename = self.asset['name']
     final_destination = os.path.join(self.download_folder, original_filename)
-    
+
+    if os.path.isfile(final_destination) and not self.replace:
+      try:
+        raise FileExistsError   # Added in python 3.3
+      except NameError:
+        raise OSError('File exists')
+
     url = self.asset['original']
-    r = requests.get(url)
-    
-    open(final_destination, 'wb').write(r.content)
-    
+    r = requests.get(url, stream=True)
+
+    handle = open(final_destination, 'wb')
+
+    for chunk in r.iter_content(chunk_size=4096):
+      if chunk:
+        handle.write(chunk)
