98227e65c1f0c873ed74b61bb60ff247bea4d152
Max Stromberg <m@maxstr.se>
2020-07-07 18:35:42 +0200

    add requests.Session() and retry strategy

--- frameioclient/download.py
+++ frameioclient/download.py
@@ -1,11 +1,20 @@
-import requests
 import os
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
 
 class FrameioDownloader(object):
   def __init__(self, asset, download_folder, replace):
     self.asset = asset
     self.download_folder = download_folder
     self.replace = replace
+    self.retry_strategy = Retry(
+        total=3,
+        backoff_factor=1,
+        status_forcelist=[429],
+        method_whitelist=['GET']
+    )
 
   def download(self):
     original_filename = self.asset['name']
@@ -13,12 +22,21 @@ class FrameioDownloader(object):
 
     if os.path.isfile(final_destination) and not self.replace:
       try:
-        raise FileExistsError   # Added in python 3.3
+        raise FileExistsError
       except NameError:
-        raise OSError('File exists')
+        raise OSError('File exists')  # Python < 3.3
+
+    adapter = HTTPAdapter(max_retries=self.retry_strategy)
+    http = requests.Session()
+    http.mount('https://', adapter)
 
     url = self.asset['original']
-    r = requests.get(url, stream=True)
+
+    r = http.request(
+      'GET',
+      url,
+      stream=True
+    )
 
     handle = open(final_destination, 'wb')
 
