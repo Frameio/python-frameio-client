06affe688b551954325f533df37369f3953eb61f
Jeff <jeff@jeffhq.com>
2020-10-23 15:20:24 -0700

    Clean up example

--- examples/new_tests.py
+++ examples/new_tests.py
@@ -6,24 +6,15 @@ from frameioclient import FrameioClient, Asset, ClientVersion
 
 token = os.getenv('FRAMEIO_TOKEN')
 client = FrameioClient(token)
-
-# print(ClientVersion.version())
-
-# Asset(token).simple_upload(
-#     'dd8526ee-2c7d-4b48-9bf7-b847664666bb', 
-#     '/Users/jeff/Code/python-frameio-client/examples/downloads/accelerated_Test_Chart_5_Sec_embedded_meta_Mezzanine.mxf'
-# )
-
-# print(client.users.get_me())
-
 folder_id = 'dd8526ee-2c7d-4b48-9bf7-b847664666bb'
 file_path = '/Users/jeff/Code/python-frameio-client/examples/downloads/accelerated_Test_Chart_5_Sec_embedded_meta_Mezzanine.mxf'
 
 client.assets.upload(folder_id, file_path)
 
-# print(client.users.get_me())
 
-# pprint(client.teams.list_projects(client.teams.list_all()[0]['id']))
+print(client.users.get_me())
 
-# for log in client.logs.list(client.users.get_me()['account_id']):
-#     print(log)
+pprint(client.teams.list_projects(client.teams.list_all()[0]['id']))
+
+for log in client.logs.list(client.users.get_me()['account_id']):
+    print(log)
