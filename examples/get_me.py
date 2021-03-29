22de64a6b3ace28f37ca0ee9e641f6ad932b4e9c
Jeff Hodges <jhodges@frame.io>
2021-03-29 14:02:06 -0700

    Update the examples to use the new client object

--- examples/get_me.py
+++ examples/get_me.py
@@ -4,7 +4,7 @@ import os
 def main():
   TOKEN = os.getenv("FRAME_IO_TOKEN")
   client = FrameioClient(TOKEN)
-  me = client.get_me()
+  me = client.users.get_me()
   print(me['id'])
 
 if __name__ == "__main__":
