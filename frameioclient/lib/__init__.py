c74d64a85d7ec235cbff65bf62206117f2b3f16b
Jeff Hodges <jhodges@frame.io>
2021-08-02 21:02:20 -0700

    Resolve conflicts post-merge

--- frameioclient/lib/__init__.py
+++ frameioclient/lib/__init__.py
@@ -6,4 +6,4 @@ from .version import ClientVersion
 from .upload import FrameioUploader
 from .download import FrameioDownloader
 from .transport import AWSClient, APIClient
-from .utils import Utils, PaginatedResponse, KB, MB
+from .utils import Utils, PaginatedResponse, KB, MB, Reference
