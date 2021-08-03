291fff211c04d67a96ccfb41f7dfaac099fd5302
Jeff Hodges <jhodges@frame.io>
2021-08-02 21:17:29 -0700

    Refactor examples, and add one more for RBC

---
+++ examples/comments/range_based_comment.py
@@ -0,0 +1,15 @@
+import os
+from frameioclient import FrameioClient
+
+def leave_range_based_comment(asset_id, comment):
+    client = FrameioClient(os.getenv("FRAME_IO_TOKEN"))
+    res = client.create_comment(
+        asset_id=asset_id,
+        text="This is my range based comment",
+        timestamp=1911,
+        duration=3.5
+    )
+
+
+if __name__ == "__main__":
+    leave_range_based_comment("id", "this is my comment!")
