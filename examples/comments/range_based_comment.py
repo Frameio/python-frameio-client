1c8a201d2b69de9f27b487cfebc6150dbe0ff83b
Jeff Hodges <jhodges@frame.io>
2021-08-02 21:18:00 -0700

    Update range-based comment example

--- examples/comments/range_based_comment.py
+++ examples/comments/range_based_comment.py
@@ -3,12 +3,14 @@ from frameioclient import FrameioClient
 
 def leave_range_based_comment(asset_id, comment):
     client = FrameioClient(os.getenv("FRAME_IO_TOKEN"))
-    res = client.create_comment(
+    res = client.comments.create(
         asset_id=asset_id,
         text="This is my range based comment",
         timestamp=1911,
         duration=3.5
     )
+
+    print(res)
 
 
 if __name__ == "__main__":
